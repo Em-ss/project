d41caaef3c23d3c1d13d48527a03d99088466136
Emil <www@com>
2021-11-06 13:27:57 +0300

    Первое приложение

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+let number0fFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovueDB = {
+    count: console.log(number0fFilms),
+    movies:"",
+    actors:"",
+    genres:[],
+    privat:false,
+
+};
+
+const movies = [];
+movies[0] = prompt("Один из просмотренных фильмов?", "");
+movies[1] = prompt("На сколько оцените его?", "");
+
+console.log(movies[0]);
+console.log(movies[1]);
+
+
